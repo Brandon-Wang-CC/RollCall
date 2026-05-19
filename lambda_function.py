9e9832af82b9d6385ebd7ac8e3092b35441060be
Charlie Meyer <69160913+GravitiMusic@users.noreply.github.com>
2026-05-18 21:09:40 -0400

    removed csv path from prefixes

--- lambda_function.py
+++ lambda_function.py
@@ -27,9 +27,9 @@ TMP_DIR = "/tmp" if os.environ.get("AWS_EXECUTION_ENV") else os.path.join(os.pat
 
 
 FILE_PREFIXES = {
-    "unfilled":   "csv/ES&F_GR&S Unfilled Requisition Report",
-    "contractor": "csv/NEW-IT Contractor-VG-Vendor Req Report-",
-    "candidates": "csv/GR&S Candidate Flow Weekly Report",
+    "unfilled":   "ES&F_GR&S Unfilled Requisition Report",
+    "contractor": "NEW-IT Contractor-VG-Vendor Req Report-",
+    "candidates": "GR&S Candidate Flow Weekly Report",
 }
 
 FILTER_CONFIG = {
